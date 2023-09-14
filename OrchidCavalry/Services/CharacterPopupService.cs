f4b5e91f4d7c6d260e001832c2339ba24f50a339
Anthony C. Jones <randomgeekdom@gmail.com>
2023-09-14 15:58:24 -0500

    Moving services under folder

--- OrchidCavalry/Services/CharacterPopupService.cs
+++ OrchidCavalry/Services/CharacterPopupService.cs
@@ -1,13 +1,12 @@
 ﻿using OrchidCavalry.Models;
 using OrchidCavalry.Popups;
-using OrchidCavalry.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrchidCavalry
+namespace OrchidCavalry.Services
 {
     public class CharacterPopupService : ICharacterPopupService
     {
